30180474263e07725d7d857bdb9532c9cf9d519b
mahmoud mabrok <mahmoudmabrok3579@gmail.com>
2018-02-17 06:57:22 +0200

    add ch24

---
+++ src/ch24/MyLinkedList.java
@@ -0,0 +1,52 @@
+package ch24;
+
+import java.util.Iterator;
+
+/**
+ * this is
+ * Created by mo3tamed on 2/13/18.
+ */
+public class MyLinkedList<E> extends  MyAbstractList<E> {
+
+
+
+    @Override
+    public void add(int index, E e) {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E e) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(E e) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(E e) {
+        return 0;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
